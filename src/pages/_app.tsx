8068f02091eded4c753dc0cbfe9e0f84a06c3309
gustavosalviato <gustavosalviato8@gmail.com>
2023-05-02 09:47:08 -0300

    chore: add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '@/styles/globals.css'
+import '../styles/global.css'
 import type { AppProps } from 'next/app'
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
