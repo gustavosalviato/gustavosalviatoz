66943114aaaed60126941fe8f7937fbc24abed82
gustavosalviato <gustavosalviato8@gmail.com>
2023-05-03 19:23:52 -0300

    :wrench: chore: apollo client configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@/lib/apollo'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
